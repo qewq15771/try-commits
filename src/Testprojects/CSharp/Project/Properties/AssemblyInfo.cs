09f9fa7f4942e8f4cda9c6cb5a8f18c46d57730b
leystofasd <leystof@gmail.com>
2024-01-22 18:47:56 +0000

    Optimize README - fixing a bug

--- src/Testprojects/CSharp/Project/Properties/AssemblyInfo.cs
+++ src/Testprojects/CSharp/Project/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject")]Optimize README - fixing a bug

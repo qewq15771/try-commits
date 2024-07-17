93fdeae576c61c5ee95425a94ebb4b279ccd4402
leystofasd <leystof@gmail.com>
2024-07-17 16:05:57 +0000

    Update API - fixing a bug

--- src/ReportGenerator.Core.Test/Parser/VisualStudioParserTest_2013.cs
+++ src/ReportGenerator.Core.Test/Parser/VisualStudioParserTest_2013.cs
@@ -184,4 +184,4 @@ namespace Palmmedia.ReportGenerator.Core.Test.Parser
                 .AnalyzeFile(new CachingFileReader(new LocalFileReader(), 0, null));
     }
 }
-Change tests - enhancing logs
+Change tests - enhancing logsUpdate API - fixing a bug

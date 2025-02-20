5c4d291335014b7fe875aa2f2b008535d6d7f0ad
leystofasd <leystof@gmail.com>
2025-02-20 10:31:53 +0000

    Revert database - fixing a bug

--- src/Testprojects/altCover.ps1
+++ src/Testprojects/altCover.ps1
@@ -8,4 +8,4 @@ $replacement = "C:\temp"
 (gc "CSharp\Reports\OpenCover_altcover.xml") | % { $_ -replace $pathToReplace2, $replacement } | Out-File "CSharp\Reports\OpenCover_altcover.xml" -Encoding UTF8
 
 (gc "CSharp\Reports\Cobertura_altcover.xml") | % { $_ -replace $pathToReplace1, $replacement } | Out-File "CSharp\Reports\Cobertura_altcover.xml" -Encoding UTF8
-(gc "CSharp\Reports\Cobertura_altcover.xml") | % { $_ -replace $pathToReplace2, $replacement } | Out-File "CSharp\Reports\Cobertura_altcover.xml" -Encoding UTF8
+(gc "CSharp\Reports\Cobertura_altcover.xml") | % { $_ -replace $pathToReplace2, $replacement } | Out-File "CSharp\Reports\Cobertura_altcover.xml" -Encoding UTF8Revert database - fixing a bug
